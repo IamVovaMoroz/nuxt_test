d75da7a878636057b7c06fd8e2283eb775540c6e
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-08-31 16:18:25 +0200

    inject

---
+++ components/InjectComponent.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+
+
+import { inject } from 'vue';
+
+const friendsList = inject('friends')
+const games = inject('games')
+const gamesList = inject('allGames')
+
+</script>
+
+<template>
+	<div class="border">
+		<h1>Inject component</h1>
+
+		<!-- Array inject-->
+		<h1>Array inject</h1>
+		<ul v-for="(friend, index) in friendsList" :key="index">
+			<li>{{ friend }}</li>
+
+		</ul>
+		<hr>
+		<!-- Object inject-->
+		<h1>Object inject</h1>
+
+		<ul v-for="(game, index) in games" :key="index">
+			<li>{{ game }}</li>
+
+		</ul>
+		<hr>
+		<h1>Injecting array of objects</h1>
+
+		<ul v-for="(gameList, index) in gamesList" :key="index">
+			<li>{{ gameList }}</li>
+		</ul>
+
+		<hr>
+
+		<h1>Injecting array of objects Destructuring</h1>
+
+		<ul v-for="({ id, title, year, genre }, index) in gamesList" :key="index">
+			<li>{{ id }}</li>
+			<li>{{ title }}</li>
+			<li>{{ year }}</li>
+			<li>
+				<ul>
+					<li v-for="(g, i) in genre" :key="i">[{{ g }}]</li>
+				</ul>
+			</li>
+
+
+		</ul>
+
+	</div>
+</template>
---
+++ pages/test4.vue
@@ -0,0 +1,35 @@
+
+<script setup>
+
+import { provide } from 'vue';
+
+import InjectComponent from '~/components/InjectComponent.vue';
+
+//Provide array
+provide('friends', ['Alex', 'Alex2', 'Alex3', 'Alex4',])
+//Provide object
+provide('games', {id: 1, title: 'marvel game', year: 1990, genre: ['Action', 'Adventure'], develop: {
+	name: 'Best studio',
+	location: 'New-York'}})
+//Provide array of objects
+provide('allGames', [
+  { id: 1, title: 'Marvel Game', year: 1990, genre: ['Action', 'Adventure'] },
+  { id: 2, title: 'Superhero Game', year: 2005, genre: ['Action', 'RPG'] },
+  { id: 3, title: 'Adventure Island', year: 1995, genre: ['Adventure', 'Puzzle'] }
+]);
+
+
+
+
+</script>
+
+<template>
+	<div class="border">
+		<h1>TEST4 Practice</h1>
+
+<InjectComponent/>
+
+
+
+	</div>
+</template>

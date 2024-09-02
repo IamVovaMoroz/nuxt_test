e9a5a17b51119921cb18b0e838372faa8e94e2ea
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-09-02 14:58:34 +0200

    life cycle components

---
+++ components/LifeCycleComponent.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue';
+
+const message =ref('Hello Lifecycle Hooks')
+
+// Lifecycle Hooks
+// onBeforeMount
+onBeforeMount(()=> {console.log(' onBeforeMount - Component is about to be mounted')})
+// onMounted
+onMounted(()=>{
+	console.log(' onMounted - Component has been mounted')
+})
+
+
+//onBeforeUpdate
+onBeforeUpdate(()=>{
+	console.log(' onBeforeUpdate - Component is about to update')
+
+})
+
+//onUpdated
+onUpdated(()=>{
+	console.log('onUpdated - Component has been updated')
+})
+
+
+//onBeforeUnmount
+
+onBeforeUnmount(()=>{
+	console.log('onBeforeUnmount - component is about to be unmounted')
+})
+
+//onUnmounted
+
+onUnmounted(()=>{
+	console.log('onUnmounted - component has been unmounted')
+})
+
+const updateMessage = ()=>{
+	message.value = 'the message was updated function'
+
+}
+
+</script>
+
+<template>
+	<div class="border">
+	<h1>LifeCycleComponent</h1>
+<p>{{ message }}</p>
+<button @click="updateMessage" class="border bg-slate-500">Update message</button>
+
+
+
+</div>
+</template>
---
+++ pages/test5.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import { ref } from 'vue';
+const showHide = ref(true)
+
+</script>
+
+<template>
+	<div class="border">
+	<h1>TEST5</h1>
+
+<LifeCycleComponent v-if="showHide"/>
+<button @click="showHide = !showHide">Show / Hide</button>
+
+</div>
+</template>
